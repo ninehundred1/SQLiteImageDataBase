7295a3788255abc9cffdf2222c18bb70c01aec3f
ninehundred1 <meyernsen@gmail.com>
2016-04-22 16:18:07 -0400

    added files

---
+++ README.md
@@ -0,0 +1,186 @@
+# Matlab Handle Class to connect to an SQLite database for storing images in tree structure with metadata attached to each image. 
+
+The images are copied into a separate folder and only the paths are stored in the database together with the metadata.
+The database can be queried for metadata parameter ranges and the image paths get returned.
+
+This current version deals with the storage and organization of differently cropped images which are stored in a tree. A region of a main image (top level of tree) gets cropped to be the second level of the tree. That level gets further croped to form the next level and so on.
+
+
+**Tree layout (image from Weizman link below):**
+
+![TREE](http://i.imgur.com/4FezCGI.jpg)
+
+
+The images are used for behavioral studies where the subject has to decide if a subimage is part of a main image (the smaller the subimage, the more difficult it becomes) and the image is analysed for features necessary to identify as belonging to a particular main image. 
+
+Check here for details:
+[ Atoms of recognition in human and computer vision (Weizmann)](http://www.wisdom.weizmann.ac.il/~dannyh/Mircs/mircs.html
+ "CAMshift") 
+ 
+
+
+**See the bottom of this file for setup (you need to download a few things and move them to the right folder, or modify the paths in the matlab file to match where you store the files).**
+
+##### A. Define the image metadata
+Each image is shown and attached to it is meta data about how often it was shown, how often correctly identified as being part of the main image, and some basic statistics about how often it should be shown to match the experimental setup. Specific images can be retrieved from the database based on thresholds selected by the user of the class (eg, show images that were correctly identified less than 70%, show images not shown this week, show images only of class *'bicycles'*, etc).
+
+The metatags are defined in the class code and can be changed to fit other needs by changing this line in the **addNewTable()** function as well as the variable **dbcolnames** in the constructor. The current version is:
+
+    createTable = ['CREATE TABLE ',this.dbTableName,'(Class varchar, '...
+                 'SubClass NUMERIC, Hum_C_Score NUMERIC, Size NUMERIC, Subset NUMERIC, '...
+               'Is_MIRC NUMERIC, Monkey_C_Score NUMERIC, Times_shown NUMERIC, Times_correct NUMERIC, '...
+               'Times_wrong NUMERIC, Date_last_shown NUMERIC, Date_added NUMERIC, '...
+            'Path VARCHAR)'];
+
+
+In this case **Class** defines the highest order branch of the tree. It can be eg *'Bike'* to include images of many different bikes, where another **Class** is *'Ships'*, etc.
+
+**Subclass** defines the different *bikes* (eg 1 for a blue bike, 2 for a racing bike, etc) and is one branch of the node **Class** (each image with the same **Subclass** ID should be derived from the same original image).
+
+**Subset** is used to add a tag to each image so you can different sub sets. *BikeBlue* and *BikeBrown* might be assigned **Subset** 1, *BikeRed* and *BikeBlack* might be assigned **Subset** 2, so you can then query for only a portion of the whole data for one week (using **Subset** 1), then have completely different images the next week (using **Subset** 2).
+
+**Hum_C_Score, Monkey_C_Score, Is_MIRC** are all extra Tags that are used to keep statistics of the image properties during the experiment.
+
+*Example Matlab command for db initialization:*
+
+    %Initialize Database and Table
+    db = SQLiteImageDataBase('koff', 'Experiment_1');
+
+
+
+
+
+##### B. Define the folder structure of source Images and Metadata CSV file and add data
+Images are added to the database by the **InsertIntoTable(Class_in, SubClass_in, Subset_in)** function, which will ask to select a folder (*ChosenFolder*). The Folder will have one original image and several subfolders (eg *images_id2/*), each subfolder being on tree branch with the images (*.png*) and also a CSV file (*hier_tree_info.csv*) with some of the metadata that will be added to each entry. 
+
+The hierachy is currently set as this:
+
+* ChosenFolder/
+  * **original.png**
+  * stimuli/
+    * images_id2/
+      * *hier_tree_info.csv*
+      * **patch_id2.png**
+      * **patch_id3.png**
+      * ...
+    * images_id4/
+    * ...
+
+As parameters of the **InsertIntoTable(Class_in, SubClass_in, Subset_in)**  function you need to specify where you want the images to be added to (which *Class*, *SubClass* and *Subset*), as there should be the option to extend all those branches later on.
+
+After a folder is selected, the file named **original.png** is copied with a unique ID into the database and added with the *tag Is_Original* set to *True*.
+Then the subfolder */stimuli* is chosen and images are read from all folders within the */stimul*i folder (*images_id2, image_id4*, etc).
+
+Each of those folders contains a CSV file with extra info (size of image, C_score, etc) which is processed together with its corresponding image in the private **InsertFolder()** function.
+
+*Example of CSV format:*
+
+| patch_filename  |  score |  size    |  father_id |  mirc_fl |
+|-----------------|--------|----------|------------|----------|
+| patch_id717.png | 2      | 18       | -1         | 0        |
+| patch_id2.png   | 0.58   | 14       | -1         | 1        |
+| patch_id12.png  | 0.3    | 11.66667 | 2          | 0        |
+| patch_id8.png   | 0.39   | 12       | 2          | 0        |
+| patch_id9.png   | 0.15   | 12       | 2          | 0        |
+
+
+
+
+####
+
+*Matlab command for image insertion:*
+
+    %Add images and Metadata to Table
+    db.InsertIntoTable('Bike_blue', 1, 2);
+
+
+**Here an example of the data within the database (using SQL browser)**
+![sql](http://i.imgur.com/HEgIQfm.jpg)
+
+
+
+
+##### C. Query the db table for different range of parameters
+Use the **RetrieveEntriesFromTable()** function to retrieve db entries within a defined range across defined columns.
+
+In the current version the db columns used are 
+* **Class** (*specify what class it belongs to, eg 'Bike'*), 
+* range of **Subclass** (between eg 1 and 4), 
+* range of **C_scores** (between eg 0.5 and 0.7) and 
+* range of **times_shown** (between eg 100 and 200).
+
+As there might be many entries you can limit the number of entries returned (**max_entries**) and what column to use for that limit (eg show only the top 100 **times_shown**).
+
+**All parameters need to be strings, and the cursor object gets returned, which can be extended by using .data.**
+
+*Matlab command for db query:*
+
+    %Query table for parameter ranges
+    db.RetrieveEntriesFromTable('Bike_blue', '0','1', '0.5','0.7' , '0', '2', '0', '-1', '0', '-1','0', 'Hum_C_Score', '50','0'  )
+    %expand the cursor to get data
+    the_data = ans.data
+
+
+
+**Here the returned data from above query**
+![TREE](http://i.imgur.com/omc37rg.jpg)
+
+
+
+The image can then be loaded using:
+
+    %get path entry for first image from query
+    imagePath = the_data(1,14);
+    %convert cell to string
+    imagePath_string = sprintf('%s\n',the_data{:})
+    %load image into Matlab
+    imread(imagePath_string)
+
+
+ ##### FOLLOW BELOW AS SETUP
+ 
+1. download SQLite3 (I used Sqlite 3.11)
+    https://www.sqlite.org/download.html
+    
+2. download driver sqlite-jdbc-3.8.11.2.jar from
+
+    https://bitbucket.org/xerial/sqlite-jdbc/downloads
+    
+    and change the path below at varible YOUR_COMPUTER_JAR_FILE to match
+    the location where you saved it
+    eg 'C:\Users\Meyer\AppData\Roaming\MathWorks\MATLAB\R2012a\sqlite-jdbc-3.8.11.2.jar'
+    
+3. make a folder in 'C:' called 'sqlite_dbs' where your databases are
+    stored
+    ('eg C:\sqlite_dbs\')
+    
+4. to view the data outside of Matlab, download SQLite browser from
+    http://sqlitebrowser.org/
+    
+
+for more help:
+
+http://www.mathworks.com/help/database/ug/sqlite-jdbc-windows.html
+    
+    EXAMPLE USE:
+    %Initialize Database and Table
+    db = SQLiteImageDataBase('koff', 'Experiment_1');
+    
+    %Add images and Metadata to Table
+    db.InsertIntoTable('Bike_blue', 1, 2);
+    
+    %Query table for parameter ranges
+    db.RetrieveEntriesFromTable('Bike_blue', '0','1', '0.5','0.7' , '0', '2', '0', '-1', '0', '-1','0', 'Hum_C_Score', '50','0'  )
+    
+    %expand the cursor to get data into the Matlab workspace as cell matrix
+    the_data = ans.data
+
+
+
+
+
+
+
+
+emails to:
+ <fuschro@gmail.com>
---
+++ SQLiteImageDataBase.m
@@ -0,0 +1,356 @@
+classdef SQLiteImageDataBase < handle
+%     Interface class to a SQLite database storing the location
+%     of images and the hierachy among the image.
+%     Images are copied into a single folder called SourceImages
+%     with no subfolder structure.
+
+    %{
+    Stephan Meyer 2016 fuschro@gmail.com
+    https://github.com/ninehundred1
+    
+    
+    Matlab Handle Class to connect to an SQLite database for storing images
+    in tree structure with metadata attached to each image. The images are
+    copied into a separate folder and only the paths are stored in the database
+    together with the metadata.
+    The database can be queried for metadata parameter ranges and the image
+    paths gets returned.
+     
+    FOLLOW BELOW AS SETUP
+    1. download SQLite3 (I used Sqlite 3.11)
+    https://www.sqlite.org/download.html
+    
+    2. download driver sqlite-jdbc-3.8.11.2.jar from
+    https://bitbucket.org/xerial/sqlite-jdbc/downloads
+    and change the path below at varible YOUR_COMPUTER_JAR_FILE to match
+    the location where you saved it
+    eg 'C:\Users\Meyer\AppData\Roaming\MathWorks\MATLAB\R2012a\sqlite-jdbc-3.8.11.2.jar'
+    
+    3. make a folder in 'C:' called 'sqlite_dbs' where your databases are
+    stored
+    ('C:\sqlite_dbs\')
+    
+    4. to view the data outside of Matlab, download SQLite browser from
+    http://sqlitebrowser.org/
+    
+    for more help:
+    http://www.mathworks.com/help/database/ug/sqlite-jdbc-windows.html
+    
+    EXAMPLE USE:
+    %Initialize Database and Table
+    db = SQLiteImageDataBase('koff', 'Experiment_1');
+    
+    %Add images and Metadata to Table
+    db.InsertIntoTable('Bike_blue', 1, 2);
+    
+    %Query table for parameter ranges
+    db.RetrieveEntriesFromTable('Bike_blue', '0','1', '0.5','0.7' , '0', '2', '0', '-1', '0', '-1','0', 'Hum_C_Score', '50','0'  )
+    
+    %expand the cursor to get data into the Matlab workspace as cell matrix
+    the_data = ans.data
+     
+    %}
+    
+    properties(GetAccess='private', SetAccess='private')
+        dbName % db Name
+        dbTableName %db Table Name
+        Connection % SqlConnection object
+        dbpath %path to database
+        URL %URL
+        SourceImageFolderPath
+        dbcolnames %db colum names
+    end
+    
+    methods
+        %{
+        ----Constructor----
+        As parameters use the name of database and table as a string
+        Args:
+            dataBaseName(String) = Name of SQLite database to connect to.
+            tableName(String) = Name of SQLite table to connect to.
+        %}
+        function this = SQLiteImageDataBase(dataBaseName, tableName)
+            this.initDB(dataBaseName, tableName);
+            this.initImageFolder(this.SourceImageFolderPath);
+            this.dbcolnames = {'Class','SubClass','Hum_C_Score','Size','Subset','Is_MIRC','Monkey_C_Score',...
+                'Times_shown', 'Times_correct', 'Times_wrong', 'Date_last_shown','Date_added', 'Is_Original', 'Path'};
+        end
+        
+        %---- BELOW ARE THE INTERFACE METHODS----
+        function Close(this)
+            %{
+            ----Close db connection----
+            %}
+            close(this.Connection);
+            disp('connection closed')
+        end
+        
+        function Open(this)
+            %{
+            ----Open db connection----
+            %}
+            this.Connection = database(this.dbpath,'','','org.sqlite.JDBC',this.URL);
+            if ~isconnection(this.Connection)
+                disp(this.dbpath);
+                error('MyComponent:noConnection',...
+                    'Error. \n Connection failed, check class header for setup instructions.')
+            else
+                disp('Connection open.')
+                disp(['Location: ',this.dbpath])
+            end
+        end
+        
+        function InsertIntoTable(this,Class_in, SubClass_in, Subset_in)
+            %{
+             ---- Make new table with these rows----
+            As Parameters use the Image Class as string (eg 'Bike'), the
+            Subclass as int (eg 2) and the Subset as int (eg 2).
+            Subclass is defined as one branch of a tree (don't use the number
+            for other branches, subset is defined as a collection of
+            branches that you can then query for (eg if you two subsets,
+            you can show only images from subset 1 one week, 2 another
+            week.
+            Args:
+            Class_in(String) = Name of Class you want the image to be in
+            (eg 'Bike')
+            SubClass_in(int) = Number of SubClass (eg 2, to be understood
+            as Bike 2)
+            Subset_in(int) = Number of Subset to be in (eg 4, to be part of
+            subset 4.
+            %}
+            folder_path = uigetdir('C:\','Select folder containing stimuli and original.');
+            if ~folder_path
+                error('no folder selected');
+            end
+            [pathstr,folder_name,ext] = fileparts(folder_path);
+            unique_ID = SQLiteImageDataBase.generate_unique_ID();
+            %copy the original to the images folder with a unique ID
+            %attached
+            try
+                ImageSavePath = SQLiteImageDataBase.copyOriginal(unique_ID,folder_name, pathstr, this.SourceImageFolderPath);
+                %add the image to database
+                dbdata = {Class_in, SubClass_in, -1, -1, Subset_in, 0, -1,...
+                    -1, -1, -1, -1, datestr(now,'yyyy-mm-dd HH:MM:SS'), 1, ImageSavePath};
+                %insert into db using the columnames defined in constructor
+                datainsert(this.Connection,this.dbTableName,this.dbcolnames,dbdata)
+                disp('copied orignal.')
+            catch
+                disp('failed to copied orignal.')
+            end
+            % Get all subfolders inside folder 'stimuli' as list and import
+            files = dir([folder_path,'/stimuli']);
+            dirFlags = [files.isdir];
+            % Extract only those that are directories.
+            subFolders = files(dirFlags);
+            for k = 1 : length(subFolders)
+                %ignore .. and . folders
+                if ~(strcmp(subFolders(k).name,'.')) &&  ~(strcmp(subFolders(k).name,'..'))
+                    SQLiteImageDataBase.InsertFolder(unique_ID,[[folder_path,'\stimuli'],'\',subFolders(k).name],...
+                        this.SourceImageFolderPath, this.Connection, this.dbTableName, Class_in, SubClass_in, Subset_in, this.dbcolnames);
+                end
+            end
+            %make query to get last 5 entrie rows of data to display in command window
+            setdbprefs('DataReturnFormat','cellarray');
+            curs = exec(this.Connection,sprintf('select * from %s ORDER BY ROWID DESC LIMIT 5', this.dbTableName),5);
+            ret_data = fetch(curs);
+            disp('********');
+            disp('last 5 rows of new entries:');
+            disp(ret_data.Data);
+            disp('****done importing images****');
+        end
+        
+        function curs = RetrieveEntriesFromTable(this, Class_in, SubClass_min, SubClass_max,...
+                Hum_C_Score_min, Hum_C_Score_max, Subset_min, Subset_max,...
+                Is_MIRC_in, Monkey_C_Score_min, Monkey_C_Score_max, Times_shown_min,...
+                Times_shown_max, Sort_By_column, max_entries, Is_original_in)
+            
+            %{
+             ---- Retrieve Entries to db using a query----
+            As Parameters use the columns you want to query and the range.
+            Eg Subset_min is the min value that gets returned for Subset,
+            Subset_max would be the max. All other parameters further
+            narrow down the query.
+                
+            ALL NEED TO BE STRINGS, eg
+            curs = db.RetrieveEntriesFromTable('Bike_blue', '0','1', '0.6','0.7' ,
+            '0', '2', '0', '-1', '0', '-1','0', 'Hum_C_Score', '50','0'  )
+
+             Returns a cursor object. To access the data, use .data on the
+             return (the_data = curs.data;)
+                %}
+                
+                sqlquery = ['SELECT * FROM ',this.dbTableName,...
+                    ' WHERE SubClass BETWEEN ',SubClass_min,' AND ',SubClass_max,...
+                    ' AND Subset BETWEEN ',Subset_min,' AND ',Subset_max,...
+                    ' AND Monkey_C_Score BETWEEN ',Monkey_C_Score_min,' AND ',Monkey_C_Score_max,...
+                    ' AND Hum_C_Score BETWEEN ',Hum_C_Score_min,' AND ',Hum_C_Score_max,...
+                    ' AND Times_shown BETWEEN ',Times_shown_min,' AND ',Times_shown_max,...
+                    ' AND Times_shown BETWEEN ',Times_shown_min,' AND ',Times_shown_max,...
+                    ' AND Is_original IS ', Is_original_in,...
+                    ' AND Class = ''', Class_in, '''',...
+                    ' AND Is_MIRC IS ', Is_MIRC_in,...
+                    ' ORDER BY ', Sort_By_column,' DESC LIMIT ',max_entries];
+                setdbprefs('DataReturnFormat','cellarray');
+                curs = exec(this.Connection,sqlquery);
+                curs = fetch(curs);
+        end
+        
+    end
+    
+    %---- BELOW ARE THE PRIVATE METHODS----
+    methods(Access = private)
+        
+        function initDB(this, dataBaseName, TableName)
+            %{
+            ----Initializes specified db/table and connects----
+            -ADJUST THE PATH TO YOUR_COMPUTER_JAR_FILE TO MATCH THE LOCATION
+            OF YOUR JAR FILE
+            -ADJUST THE PATH TO SourceImageFolderPath TO MATCH WHERE YOU
+            WANT YOUR DATABASE TO BE STORED
+            Args:
+            dataBaseName(String) = Name of SQLite database to connect to.
+            If doesn't exist, a new will be generated
+            tableName(String) = Name of SQLite table to connect to.
+            If doesn't exist, a new will be generated
+            %}
+            YOUR_COMPUTER_JAR_FILE=...
+                'C:\Users\Meyer\AppData\Roaming\MathWorks\MATLAB\R2012a\sqlite-jdbc-3.8.11.2.jar';
+            this.SourceImageFolderPath = 'C:\sqlite_dbs\';
+            javaaddpath(YOUR_COMPUTER_JAR_FILE)
+            this.dbName = dataBaseName;
+            this.dbTableName = TableName;
+            this.dbpath = [this.SourceImageFolderPath,dataBaseName,'.db'];
+            this.URL=['jdbc:sqlite:C:\sqlite_dbs\',dataBaseName,'.db'];
+            this.Open();
+            this.checkIfTableExists();
+        end
+        
+        function checkIfTableExists(this)
+            %{
+            ----Check if Table exists, if not make new----
+            %}
+            setdbprefs('DataReturnFormat','numeric');
+            qTimeout = 5;
+            %make query to get first row of data
+            curs = exec(this.Connection,...
+                sprintf('select * from %s ORDER BY ROWID ASC LIMIT 1', this.dbTableName),qTimeout);
+            ret_data = fetch(curs);
+            %if doesn't exist, cursor will return 0, if exist either data
+            %or 'No Data'. Use error handling to deal with different data types
+            %(string vs int)
+            try
+                if ret_data.data == 0
+                    %make new table if not present
+                    fprintf('adding new table: %s',this.dbTableName);
+                    this.addNewTable();
+                end
+            catch
+                fprintf('using existing table: %s',this.dbTableName);
+            end
+        end
+        
+        function this = addNewTable(this)
+            %{
+            ----Make new table with these rows----
+            %}
+            createTable = ['CREATE TABLE ',this.dbTableName,'(Class varchar, '...
+                'SubClass NUMERIC, Hum_C_Score NUMERIC, Size NUMERIC, Subset NUMERIC, '...
+                'Is_MIRC NUMERIC, Monkey_C_Score NUMERIC, Times_shown NUMERIC, Times_correct NUMERIC, '...
+                'Times_wrong NUMERIC, Date_last_shown NUMERIC, Date_added NUMERIC, Is_Original NUMERIC, '...
+                'Path VARCHAR)'];
+            exec(this.Connection,createTable)
+        end
+        
+    end
+    
+    
+    methods(Static = true)
+        function initImageFolder(SourceImageFolderPath)
+            %{
+            ----Create new image folder if not exist, and count images----
+            Args:
+            SourceImageFolderPath(String) = Path to folder.
+            %}
+            img_dir = [SourceImageFolderPath,'SourceImageFolder\'];
+            mkdir(img_dir);
+            d = dir([img_dir, '\*.png']);
+            Folder = dir([img_dir, '\*.set']);
+            NumImages=length(d);
+            disp(['PNG Images found in source folder: ', num2str(NumImages)])
+        end
+        
+        function unique_ID = generate_unique_ID()
+            %{
+            ----Create unique ID based on current time as serial----
+            %}
+            serialDate = floor(now)*10000;
+            serialTime = rem(now,1)*10000;
+            unique_ID = floor(serialDate+serialTime);
+        end
+        
+        function file_dest = copyOriginal(unique_ID,folder_name, pathstr, SourceImageFolderPath)
+            %{
+            ----Copy original image from selected folder to ImageFolder----
+            Args:
+            unique_ID(int) = unique ID to append to filename
+            folder_name, SourceImageFolderPath(String) = Path to folders.
+            %}
+            new_name_original = [folder_name,'_original_',num2str(unique_ID),'.png'];
+            file_origin = [pathstr,'\',folder_name,'\original.png'];
+            file_dest = [SourceImageFolderPath,'SourceImageFolder\',new_name_original];
+            disp(file_dest)
+            copyfile(file_origin,file_dest);
+        end
+        
+        function [ImageSavePath] = copyImage(currentImage, folder_path, SourceImageFolderPath, unique_ID)
+            %{
+            ----Copy other images from loop----
+            Args:
+            currentImage(String) = current image name
+            unique_ID(int) = unique ID to append to filename
+            folder_name, SourceImageFolderPath(String) = Path to folders.
+            %}
+            currentImagePath = [folder_path,'\',currentImage];
+            ImageSaveName =  [currentImage(1:[end-4]),'_',num2str(unique_ID),'.png'];
+            ImageSavePath = [SourceImageFolderPath,'SourceImageFolder\',ImageSaveName];
+            copyfile(currentImagePath,ImageSavePath);
+            disp(['copied file: ',currentImage]);
+        end
+        
+        function InsertFolder(unique_ID,folder_path, SourceImageFolderPath, Connection,...
+                dbTableName,Class_in, SubClass_in, Subset_in, dbcolnames)
+            %{
+            ----Insert all images of current folder into db----
+            Args:
+            currentImage(String) = current image name
+            unique_ID(int) = unique ID to append to filename
+            Connection = db connection object
+            dbTableName(string) = name of table to add data to
+            SubClass_in(string) = name of Subclass to add data to
+            Subset_in(string) = name of Subset to add data to
+            dbcolnames(cell) = cell of rows to add data to
+            folder_path, SourceImageFolderPath(String) = Path to folders.
+                %}
+                
+                %load csv file to get the meta data from
+                [num char raw] = xlsread([folder_path,'\hier_tree_info.csv']);
+                
+                %process each image ignoring header
+                for i = 2 :  size(raw,1)
+                    %copy image into source image folder
+                    ImageSavePath = SQLiteImageDataBase.copyImage(raw{i,1}, folder_path, SourceImageFolderPath, unique_ID);
+                    %grab other info from csv file
+                    currentScore = raw{i,2};
+                    currentSize = raw{i,3};
+                    currentIsMIRC = raw{i,5};
+                    %create entry into db as cell
+                    dbdata = {Class_in, SubClass_in, currentScore, currentSize, Subset_in, currentIsMIRC, -1,...
+                        -1, -1, -1, -1, datestr(now,'yyyy-mm-dd HH:MM:SS'), 0, ImageSavePath};
+                    %insert into db using the columnames defined in constructor
+                    datainsert(Connection,dbTableName,dbcolnames,dbdata)
+                end
+        end
+        
+    end
+    
+end
